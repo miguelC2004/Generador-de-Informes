5db796530292fc1afc8ab61fe838a1a92f47176a
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-02-28 10:10:52 -0500

    Create ReportGenerator.h

---
+++ ReportGenerator.h
@@ -0,0 +1,12 @@
+#ifndef REPORT_GENERATOR_H
+#define REPORT_GENERATOR_H
+
+#include <iostream>
+#include <string>
+
+class ReportGenerator {
+public:
+    virtual void generateReport() = 0;
+};
+
+#endif // REPORT_GENERATOR_H
